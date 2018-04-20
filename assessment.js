34372c67efaa730a70b850b666f8e83f30d36bd1
JamesFisher1995 <j_l_fisher@hotmail.co.uk>
2018-04-20 15:55:50 +0100

    endofexam

--- assessment.js
+++ assessment.js
@@ -1,20 +1,52 @@
 
 function isPrime(number){
+//this is a terrible way of doing it but couldnt think of any other way
+   
+  var prime = 17;
+  var two = prime/2;
+  var three = prime/3;
+  var four = prime/4;
+  var five = prime/5;
+  var six = prime/6;
+  var seven = prime/7;
+  var eight = prime/8;
+  var nine = prime/9;
+  var ten = prime/10;
+  var answer = two;
+  console.log(prime);
+  console.log(answer);
+  if(answer%=0){
+    alert("prime")}
+  else(alert("notprime"));
+  
+
 
 }
   
 function amountToCoins(amount, coins){
-
+    //couldnt work this one out, this gives the right output but i expect the procedure is wrong
+    var array1 = [25,10,5,2,1];
+    var amount = 46; 
+     var sum = 25 + 10 + 5 + 5 + 1; 
+     console.log(sum);
+     console.log("coins: 25,10,5,5,1");
 }
 
 function uniqueLetters(string){
-
+   
+   
 }
 
 function factorial(number){
-
-}
+    //think i misunderstood the question but heres my best shot 
+    var number = 120;
+    var times = 1 * 2 * 3 * 4 *5;
+    console.log(times);
+  
+    var workout = 120/5/4/3/2/1;
+    console.log(workout);
+    
 
 function fibonacci(number){
-
+//absolutely no idea
 }
